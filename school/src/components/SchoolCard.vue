d8a4675edcaef06e3f914d3cdbf43fc07c75cf6b
stella785 <stellac35@nycstudents.net>
2026-03-16 15:04:10 -0400

    ..

---
+++ school/src/components/SchoolCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-link :to="schoolpath" class="card">
+        <h2>{{  }}</h2>
+        <h3>{{  }}</h3>
+    </router-link>
+</template>
+
+<script setup>
+import {computed} from 'vue'
+const props = defineProps({
+    school:{
+        type: String,
+        required: true,
+    },
+
+})
+
+</script>
+
+<style scoped>
+
+</style>
--- school/src/views/MainScreen.vue
+++ school/src/views/MainScreen.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>
-
+    <div class="container">
+        <SchoolCard/>
     </div>
 </template>
 
 <script setup>
-
+import {ref, onMounted} from 'vue'
+import SchoolCard from '@/components/SchoolCard.vue';
+const school = ref([])
+async function getSchool() {
+    try {
+        const response = await fetch('https://data.cityofnewyork.us/resource/k8ah-28f4.json')
+        const data = await response.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+onMounted(() => {
+    getSchool()
+})
 </script>
 
 <style scoped>
