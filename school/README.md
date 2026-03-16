7f055c312ac0072be0d628d233c31d66c6d425c4
stella785 <stellac35@nycstudents.net>
2026-03-16 14:02:13 -0400

    .

--- school/README.md
+++ school/README.md
@@ -1,4 +1,4 @@
-# health
+# school
 
 This template should help get you started developing with Vue 3 in Vite.
 
@@ -35,10 +35,4 @@ npm run dev
 
 ```sh
 npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
 ```
---
+++ school/src/router/index.js
@@ -0,0 +1,27 @@
+import SchoolCard from '@/component/SchoolCard.vue'
+import SchoolData from '@/component/SchoolData.vue'
+import MainScreen from '@/views/MainScreen.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/mainscreen',
+      name: 'mainscreen',
+      component: MainScreen,
+    },
+    {
+      path: '/schooldata',
+      name: 'schooldata',
+      component: SchoolData,
+    },
+    {
+      path: '/schoolcard',
+      name: 'schoolcard',
+      component: SchoolCard,
+    }
+  ],
+})
+
+export default router
