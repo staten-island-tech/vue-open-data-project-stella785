f2ad489dceaaa3037370904577b554ebb82a8ffb
stella785 <stellac35@nycstudents.net>
2026-03-23 14:01:53 -0400

    .

---
+++ school/src/components/SchoolCharts.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Bar
+            id=""
+            :options="chartOptions"
+            :data="chartData"
+        />
+    </div>
+</template>
+
+<script setup>
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+
+</script>
+
+<style scoped>
+
+</style>
